c215034f140082e25f04337ffe09dda6488ece5d
MirsonCamadro <kztempus@gmail.com>
2019-09-30 12:24:29 -0300

    added users list to show view

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,28 @@
 class TodosController < ApplicationController
 before_action :authenticate_user!
+before_action :find_todo, only: [:show, :update]
 
   def index
-    @todos = current_user.tasks
+    @todos = current_user.todos
   end
 
   def show
-    todo = Todo.find(params[:todo_id])
-    @task = todo.task
+    @task = @todo_who.task
     @todos_completed = Todo.where(task_id: @task.id, completed: true)
   end
+
+  def update
+    @todo_who.completed = !@todo_who.completed
+    @todo_who.save
+    redirect_to todos_path
+  end
+
+  private
+  
+  def find_todo
+    @todo_who = Todo.find(params[:todo_id])
+  end
+
+
+
 end
