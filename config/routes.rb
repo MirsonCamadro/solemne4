a4cd35bf884f56a89eddc26ec1b075c789a2db3b
MirsonCamadro <kztempus@gmail.com>
2019-09-30 10:45:18 -0300

    added index view to visitors with 9 tasks generated by db:seed, gem faker and picsum

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'todos/show'
   get 'tasks/index'
   devise_for :users
+
+  root to:'tasks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Task.destroy_all
+
+require 'faker'
+
+9.times do |i|
+    Task.create(
+        title: Faker::ChuckNorris.fact,
+        photo: "https://picsum.photos/id/#{i+17}/400/400"
+    )
+end
